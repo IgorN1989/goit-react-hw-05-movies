239113dffcf5c05105888764ecfcccd142877a02
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-11-24 01:33:51 +0200

    Update MoviesList.styled.jsx

--- src/components/MoviesList/MoviesList.styled.jsx
+++ src/components/MoviesList/MoviesList.styled.jsx
@@ -5,7 +5,4 @@ export const List = styled.ul`
   max-width: calc(100vw - 48px);
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-gap: ${p => p.theme.spacing(4)};
-  padding: 0;
-  margin-top: ${p => p.theme.spacing(2)};
-  margin-right: ${p => p.theme.spacing(2)};
 `;
