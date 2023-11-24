148cde432f0a80b4585efab073a616dcd15e65ae
IgorN1989 <107757685+IgorN1989@users.noreply.github.com>
2023-11-24 03:15:04 +0000

    Deploying to gh-pages from @ IgorN1989/goit-react-hw-05-movies@baffd96f9581b0c9d0d8e7b3849306a3057c5a1e 🚀

---
+++ static/js/692.02fcfbdf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[692],{7692:function(t,e,a){a.d(e,{gew:function(){return h}});var r=a(9983);function h(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M4 21h1V8H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2zM20 8h-7l1.122-3.368A2 2 0 0 0 12.225 2H12L7 7.438V21h11l3.912-8.596L22 12v-2a2 2 0 0 0-2-2z"}}]})(t)}}}]);
+//# sourceMappingURL=692.02fcfbdf.chunk.js.map
